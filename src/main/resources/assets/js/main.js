6b43bc9354986726d94f6706d49cc12750e51f75
SpencerDo <magicdo7@gmail.com>
2015-09-09 16:26:32 -0700

    fixes a bug where does not load list of vocabularies by clicking refresh button.

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -2,12 +2,9 @@ Polymer({
     is: 'main-layout',
     ready: function() {
 	console.log('ready');
-	handleClick();
     },
     handleClick: function() {
-	this.$.ajax.url = "http://google.com";
-	this.$.ajax.params = {};
-	this.$.ajax.generateRequest();
+	this.$.vocalist.generateRequest();
     },
     hresponse: function(req) {
 	console.log('res: ' + JSON.stringify(req.detail.response));
